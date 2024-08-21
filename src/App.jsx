200d2bd07403dcd8994e7a9385fc548705d3f975
Camila Alves <alvescamila87@gmail.com>
2024-08-21 13:44:43 -0300

    style card

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,44 @@ import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
 import Aleatorio from './components/basicos/Aleatorio'
+import Card from "./components/layout/Card"
+import './App.css'
 
 // funcao nomeada: export default function App(props){}
 // funcao anônima: export default function(props){}
 // arrow function: export default (props) => {}
 
-export default _ => // {
-    //return (
-        (
-        <div id="app">
-        <h1>Fundamento React (Arrow)</h1>
-        <Aleatorio
-            min={1}
-            max={60}
-        >           
-        </Aleatorio>
-        <Fragmento/>
-        <ComParametro
-            titulo="Situação do aluno"
-            subtitulo="Dados"
-            aluno="Zebedeu Abraão"
-            nota={6.9}
-        />
-        <ComParametro
-            titulo="Situação do aluno"
-            subtitulo="Dados"
-            aluno="Maria Madalena"
-            nota={10.0}
-        />
-        <Primeiro/>
+export default () =>  (
+        <div className="App">
+            <h1>Fundamentos React</h1>
+
+            <div className="Cards">
+                <Card titulo="#04 - Desafio Aleatório" color="#FA6900">    
+                    <Aleatorio        
+                        min={1}
+                        max={60}
+                    /> 
+                </Card>
+                <Card titulo="#03 - Fragmento" color="#E94C6F">
+                    <Fragmento/>
+                </Card>
+                <Card titulo="#02 - Com Parâmetro" color="#E8B71A">
+                <ComParametro
+                    titulo="Situação do aluno"
+                    subtitulo="Dados"
+                    aluno="Zebedeu Abraão"
+                    nota={6.9}
+                />
+                <ComParametro
+                    titulo="Situação do aluno"
+                    subtitulo="Dados"
+                    aluno="Maria Madalena"
+                    nota={10.0}
+                />
+                </Card>
+                <Card titulo="#01 - Primeiro componente" color="#588C73">        
+                    <Primeiro/>
+                </Card> 
+            </div>        
         </div>
-        );
-    //);
-//}
+);
