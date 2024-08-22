f0f964e626aecc93fa202939e6557a5488aac998
Camila Alves <alvescamila87@gmail.com>
2024-08-22 13:35:11 -0300

    uncontrolled component

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import ParOuImpar from "./components/condicional/ParOuImpar"
 import UsuarioInfo from "./components/condicional/UsuarioInfo"
 import DiretaPai from "./components/comunicacao/DiretaPai"
 import IndiretaPai from "./components/comunicacao/IndiretaPai"
+import Input from "./components/formulario/Input"
 
 
 // funcao nomeada: export default function App(props){}
@@ -24,6 +25,9 @@ export default () =>  (
             <h1>Fundamentos React</h1>
 
             <div className="Cards">
+                <Card titulo="#11 - Componente controlado (Input)" color="#E45F56">
+                    <Input></Input>
+                </Card>
                 <Card titulo="#10 - Comunicação indireta" color="#8BAD39">
                     <IndiretaPai/>
                 </Card>
