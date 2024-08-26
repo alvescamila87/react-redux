7b15d1e8073d49c8c0565e55c042611ec21b8cd7
Camila Alves <alvescamila87@gmail.com>
2024-08-26 13:40:50 -0300

    useRef merge strings

--- hooks/src/views/examples/UseRef.jsx
+++ hooks/src/views/examples/UseRef.jsx
@@ -2,6 +2,12 @@ import React, { useEffect, useRef, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 
+const merge = function(s1, s2) {
+    return [...s1].map(function(elemento, indice) {
+        return `${elemento}${s2[indice] || ""}`
+    }).join("")
+}
+
 const UseRef = (props) => {
 
     // Ex #01
@@ -41,7 +47,7 @@ const UseRef = (props) => {
             <div className="center">
                 <div>
                     <span className="text">Valor: </span>
-                    <span className="text">{value1} [</span>
+                    <span className="text">{merge(value1, value2)} [</span>
                     <span className="text red">{count.current}</span>
                     <span className="text">]</span>
                 </div>
