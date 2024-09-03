cfed21122dc2adb16d61123a995a8dc74202b7ca
Camila Alves <alvescamila87@gmail.com>
2024-09-03 10:24:17 -0300

    create custom color in card, using props and function getColor: red, green, blue, purple, where header and content are className

--- redux-simples/src/App.js
+++ redux-simples/src/App.js
@@ -7,14 +7,12 @@ function App() {
     <div className="App">
       <h1>Exercício React-Redux (Simples)</h1>
       <div className='linha'>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
+        <Card title="Card 1" red>Teste X</Card>
+        <Card title="Card 2" green>Teste X</Card>
       </div>      
       <div className='linha'>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
+        <Card title="Card 3" blue>Teste Y</Card>
+        <Card title="Card 4" purple>Teste Y</Card>
       </div>      
     </div>
   );
