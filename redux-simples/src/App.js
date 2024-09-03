560e01d31fa85adab2b3f9ef8d59b4ebb6ee04bc
Camila Alves <alvescamila87@gmail.com>
2024-09-03 10:49:59 -0300

    create a range component to put min and max numbers and associate to app

--- redux-simples/src/App.js
+++ redux-simples/src/App.js
@@ -1,16 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import Card from './components/Card';
+import Intervalo from './components/Intervalo';
 
 function App() {
   return (
     <div className="App">
       <h1>Exercício React-Redux (Simples)</h1>
       <div className='linha'>
-        <Card title="Card 1" red>Teste X</Card>
-        <Card title="Card 2" green>Teste X</Card>
+        <Intervalo></Intervalo>        
       </div>      
       <div className='linha'>
+        <Card title="Card 2" green>Teste X</Card>
         <Card title="Card 3" blue>Teste Y</Card>
         <Card title="Card 4" purple>Teste Y</Card>
       </div>      
--- redux-simples/src/components/Card.jsx
+++ redux-simples/src/components/Card.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import './Card.css'
 
+/**
+ * Aplicar cor no card de acordo com a prop: Card + props
+ */
 function getColor(props) {
     if(props.red) return "Red"
     if(props.green) return "Green"
