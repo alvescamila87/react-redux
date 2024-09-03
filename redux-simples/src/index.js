2c34e566c7ba853719cd56abe812936eeb578221
Camila Alves <alvescamila87@gmail.com>
2024-09-03 13:30:41 -0300

    config first steps with redux: store config

--- redux-simples/src/index.js
+++ redux-simples/src/index.js
@@ -4,11 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// integrar react com redux
+import { Provider }from 'react-redux'
+import storeConfig from './store/storeConfig';
+
+//chama os reducers via createStore()
+const store = storeConfig()
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
