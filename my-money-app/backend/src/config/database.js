05b9187d21883eb0c9b883e22b048d23ad93f3a3
Camila Alves <alvescamila87@gmail.com>
2024-09-04 16:44:49 -0300

    config mongoDB, create schema related to the credit and debt in mongoose

---
+++ my-money-app/backend/src/config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+
+const mongoURI = 'mongodb://localhost:27017/mymoney';
+
+const connectToDatabase = () => {
+    mongoose.connect(mongoURI, {
+        useNewURLParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error: ', err));
+};
+
+module.exports = connectToDatabase;
