76f3aa37573290f8c087f6aa956f7ddf732f3fd4
Camila Alves <alvescamila87@gmail.com>
2024-09-05 09:11:46 -0300

    api test with postman and mondoBD

--- my-money-app/backend/src/config/database.js
+++ my-money-app/backend/src/config/database.js
@@ -15,5 +15,8 @@ const connectToDatabase = () => {
 
 module.exports = connectToDatabase;
 
-// exibir mensagem no PT-BR no mongoBD
+// exibir mensagens de validação em PT-BR no mongoBD
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
+mongoose.Error.messages.String.enum = "O '{VALUE}' não é válido para o atributo '{PATH}'."
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
