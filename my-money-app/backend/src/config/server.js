b808bb7d820c47b72b3f653995d594f2156a1f53
Camila Alves <alvescamila87@gmail.com>
2024-09-05 11:10:35 -0300

    config middleware query parseInt to do pagination in get query

--- my-money-app/backend/src/config/server.js
+++ my-money-app/backend/src/config/server.js
@@ -4,10 +4,12 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 const allowCORS = require('./cors')
+const queryParser = require('express-query-int') // converter string para valor numérico consulta get
 
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
 server.use(allowCORS)
+server.use(queryParser())
 
 server.listen(port, function() {
     console.log(`BACKEND is running on port ${port}.`)
