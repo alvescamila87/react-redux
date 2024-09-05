2096d38313a9255acfa6be7b4329cfbe71901ca2
Camila Alves <alvescamila87@gmail.com>
2024-09-05 11:00:49 -0300

    config cors and bind to server

---
+++ my-money-app/backend/src/config/cors.js
@@ -0,0 +1,8 @@
+//middleware
+
+module.exports = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next() //chamar o próximo middleware OU responder a requisição OU retornar mensagem de erro, senão fica parado
+}
