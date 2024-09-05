e9040b91434f90d855e71b880062280e655d65b5
Camila Alves <alvescamila87@gmail.com>
2024-09-05 16:30:03 -0300

    create footer component and bind in app

---
+++ my-money-app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2024
+            <a href="http://alvescamila87.github.io/portfolio/" target="_blank"> ALVESCAMILA87 Porfolio</a>.
+        </strong>
+    </footer>
+)
