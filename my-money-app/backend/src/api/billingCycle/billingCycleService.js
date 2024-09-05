b1856b3dc6557fca95f7d4b648bc9012363cd53a
Camila Alves <alvescamila87@gmail.com>
2024-09-05 09:25:24 -0300

    update related to the version mongoose and mongoDB and create new billingCycleService to count registers

--- my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -7,12 +7,41 @@ const BillingCycle = require('./billingCycle')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
 
+// atualização 1 em relação ao curso
 BillingCycle.route('get', (req, res, next) => {
     BillingCycle.find({}, (err, docs) => {
         if(!err) {
             res.json(docs)
         } else {
             res.status(500).json({errors: [error]})
+        }
+    })
+})
+
+// atualização 2 em relação ao curso
+BillingCycle.route('summary', (req, res, next) => {
+    BillingCycle.aggregate([{ 
+        $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}} 
+    }, { 
+        $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
+    }, { 
+        $project: {_id: 0, credit: 1, debt: 1}
+    }], (error, result) => {
+        if(error) {
+            res.status(500).json({errors: [error]})
+        } else {
+            res.json(result[0] || {credit: 0, debt: 0})
+        }
+    })
+})
+
+// serviço customizado: count (quantidade de registros)
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((error, value) => {
+        if(error) {
+            res.status(500).json({errors: [error]})
+        } else {
+            res.json({value})
         }
     })
 })
