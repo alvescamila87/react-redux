5aa5ef351d518ddb9948b31ec1c3c456c4f1e32f
Camila Alves <alvescamila87@gmail.com>
2024-09-06 16:44:13 -0300

    create dashboard2 to use without redux

--- my-money-app/frontend/src/dashboard2/dashboard2.jsx
+++ my-money-app/frontend/src/dashboard2/dashboard2.jsx
@@ -1,27 +1,36 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
+import axios from 'axios';
 
 import ContentHeader from "../common/template/contentHeader";
 import Content from "../common/template/content";
 import ValueBox from "../common/widget/valueBox";
 import Row from "../common/layout/row";
-import { getSummary } from "./dashboardActions";
 
-//com REDUX
-class Dashboard extends Component {
+const BASE_URL = 'http://localhost:3003/api'
 
+//sem REDUX
+export default class Dashboard extends Component {
+
+    //estado do objeto - sem redux
+    constructor(props) {
+        super(props)
+        this.state = { credit: 0, debt: 0}
+    }
+
+    //sem redux
     componentWillMount() {
-        this.props.getSummary()
+        axios.get(`${BASE_URL}/billingCycles/summary`)
+            .then(resp => this.setState(resp.data))
     }
 
     render() {
 
-        const { credit, debt } = this.props.summary
+        //sem redux
+        const { credit, debt } = this.state
 
         return (
             <div>
-                <ContentHeader title="Dashboard" small="Versão 1.0" />
+                <ContentHeader title="Dashboard" small="Versão 2.0" />
                 <Content>
                     <Row>
                         <ValueBox 
@@ -52,10 +61,3 @@ class Dashboard extends Component {
     }
 }
 
-const mapStateToProps = state => ({
-    summary: state.dashboard.summary
-})
-
-const mapDispatchToProps = dispatch => bindActionCreators({getSummary}, dispatch)
-
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
--- my-money-app/frontend/src/main/routes.jsx
+++ my-money-app/frontend/src/main/routes.jsx
@@ -3,7 +3,9 @@ import { Router, Route, Redirect, hashHistory } from 'react-router'
 
 import BillingCycle from "../billingCycle/billingCycle";
 import Dashboard from "../dashboard/dashboard";
+//import Dashboard2 from "../dashboard2/dashboard2";
 
+//com REDUX
 export default props => (
     <Router history={hashHistory}>
         <Route path="/" component={Dashboard}/>
@@ -11,3 +13,12 @@ export default props => (
         <Redirect from="*" to="/" />
     </Router>
 )
+
+// sem REDUX
+// export default props => (
+//     <Router history={hashHistory}>
+//         <Route path="/" component={Dashboard2}/>
+//         <Route path="/billingCycles" component={BillingCycle}/>
+//         <Redirect from="*" to="/" />
+//     </Router>
+// )
