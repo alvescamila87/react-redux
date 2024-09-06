bbbaea560cd130d4326544ca1b1b24015dc68fb6
Camila Alves <alvescamila87@gmail.com>
2024-09-06 20:08:56 -0300

    create connection between tabHeader and BillingCycle with Redux

--- my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -1,5 +1,7 @@
 // ciclos de pagamentos
 import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
@@ -7,8 +9,15 @@ import Tabs from '../common/tab/tabs'
 import TabsContent from "../common/tab/tabsContent";
 import TabsHeader from "../common/tab/tabsHeader";
 import TabHeader from "../common/tab/tabHeader"
+import TabContent from '../common/tab/tabContent'
+import { selectTab } from '../common/tab/tabActions'
 
 class BillingCycle extends Component {
+
+    // entrar na tab List por padrão
+    componentWillMount() {
+        this.props.selectTab('tabList')
+    }
 
     render() {
         return (
@@ -23,7 +32,10 @@ class BillingCycle extends Component {
                             <TabHeader label='Excluir' icon="trash-o" target="tabDelete"/>
                         </TabsHeader>
                         <TabsContent>
-
+                            <TabContent id='tabList'><h1>Listar</h1></TabContent>
+                            <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
+                            <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
+                            <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
                         </TabsContent>
                     </Tabs>
                 </Content>
@@ -32,4 +44,8 @@ class BillingCycle extends Component {
     }
 }
 
-export default BillingCycle
+
+//const mapStateToProps = state => ({})
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab}, dispatch)
+
+export default connect(null, mapDispatchToProps)(BillingCycle)
