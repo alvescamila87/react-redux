0942602a0e1de3a3c0323c57a2be32da0749646a
Camila Alves <alvescamila87@gmail.com>
2024-09-07 20:11:31 -0300

    create action getList and a reducer to bind to global reducer from the app

--- my-money-app/frontend/src/main/reducers.jsx
+++ my-money-app/frontend/src/main/reducers.jsx
@@ -1,12 +1,14 @@
-// módulo para combinar todos os reducers da aplicação
+// módulo para combinar todos os reducers da aplicação (estado global)
 import { combineReducers } from "redux";
 
 import dashboardReducer from "../dashboard/dashboardReducer";
 import tabReducer from "../common/tab/tabReducer";
+import billingCycleReducer from "../billingCycle/billingCycleReducer";
 
 const rootReducer = combineReducers({
     dashboard: dashboardReducer,
-    tab: tabReducer
+    tab: tabReducer,
+    billingCycle: billingCycleReducer
 })
 
 export default rootReducer
