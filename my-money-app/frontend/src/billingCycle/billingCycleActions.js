cfe21bb6c4313d1d9a0daab6174a51533215d5f7
Camila Alves <alvescamila87@gmail.com>
2024-09-07 21:07:06 -0300

    create renderRows to show data within tbody from billingCycleList

--- my-money-app/frontend/src/billingCycle/billingCycleActions.js
+++ my-money-app/frontend/src/billingCycle/billingCycleActions.js
@@ -1,7 +1,7 @@
 // todos os action creators responsáveis por criar as ações
 
 import axios from 'axios'
-const BASE_URL = 'http//localhost:3003/api'
+const BASE_URL = 'http://localhost:3003/api'
 
 export function getList() {
     const request = axios.get(`${BASE_URL}/billingCycles`)
