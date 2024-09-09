091faaf55d656677b2f713339cfd9249a906df9f
Camila Alves <alvescamila87@gmail.com>
2024-09-09 13:35:34 -0300

    change a in menuItem and menuTree to Link and bind to app and index

--- my-money-app/frontend/src/main/routes.jsx
+++ my-money-app/frontend/src/main/routes.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Router, Route, Redirect, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
 
+import App from './app'
 import BillingCycle from "../billingCycle/billingCycle";
 import Dashboard from "../dashboard/dashboard";
 //import Dashboard2 from "../dashboard2/dashboard2";
@@ -8,8 +9,10 @@ import Dashboard from "../dashboard/dashboard";
 //com REDUX
 export default props => (
     <Router history={hashHistory}>
-        <Route path="/" component={Dashboard}/>
-        <Route path="/billingCycles" component={BillingCycle}/>
+        <Route path="/" component={App}>
+            <IndexRoute component={Dashboard}/>
+            <Route path="billingCycles" component={BillingCycle}/>
+        </Route>        
         <Redirect from="*" to="/" />
     </Router>
 )
