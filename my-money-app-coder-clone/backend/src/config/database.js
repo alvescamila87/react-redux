0dcab341a7fefd1bb3c91803d750db90da2ececb
Camila Alves <alvescamila87@gmail.com>
2024-10-25 20:11:38 -0300

    install mongo, change database and packagejson

--- my-money-app-coder-clone/backend/src/config/database.js
+++ my-money-app-coder-clone/backend/src/config/database.js
@@ -1,11 +1,22 @@
+// mongoose: integração entre mongoBD e express
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/mymoney')
 
+const mongoURI = 'mongodb://localhost:27017/mymoney';
+
+const connectToDatabase = () => {
+    mongoose.connect(mongoURI, {
+        useNewURLParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error: ', err));
+};
+
+module.exports = connectToDatabase;
+
+// exibir mensagens de validação em PT-BR no mongoBD
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
-mongoose.Error.messages.Number.min = 
-    "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
-mongoose.Error.messages.Number.max = 
-    "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
-mongoose.Error.messages.String.enum = 
-    "'{VALUE}' não é válido para o atributo '{PATH}'."
+mongoose.Error.messages.String.enum = "O '{VALUE}' não é válido para o atributo '{PATH}'."
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
