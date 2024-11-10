7960765c4eafa8e1c95692603a6c46f7a77655fc
Camila Alves <alvescamila87@gmail.com>
2024-11-10 20:44:17 -0300

    map with json parse

--- javascript/funcao/map2.js
+++ javascript/funcao/map2.js
@@ -1,4 +1,15 @@
 const carrinho = [
-    '{ "nome": "Borracha, "preco": 3.45}',
-    '{ "nome": "Caderno, "preco": 13.90}',
+    '{ "nome": "Borracha", "preco": 3.45 }',
+    '{ "nome": "Caderno", "preco": 13.90 }',
+    '{ "nome": "Kit de lápis", "preco": 41.22} ',
+    '{ "nome": "Caneta", "preco": 7.50} ',
 ]
+
+// retornar apenas os preços
+
+// converte texto para objeto
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map(paraObjeto) //.map(apenasPreco)
+console.log(resultado)
