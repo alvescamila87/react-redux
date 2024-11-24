c3183ecee68bc741837299dec2cfe89ba77f8263
Camila Alves <alvescamila87@gmail.com>
2024-11-24 20:57:06 -0300

    middleware express

---
+++ fundamentos-express/ex02_middleware.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const server = express()
+
+server.get('/', function(req, res, next) {
+    console.log("Início...")
+    next() // chama o próximo middleware
+    console.log("Fim...")
+})
+
+server.get("/", function(req, res){
+    console.log("Resposta...")
+    res.send('<h1>Olá express</h1>')
+})
+
+server.listen(3000, () => console.log("Executando..."))
