217a8536655e28134d5085d601c102235391c1aa
Camila <alvescamila87@gmail.com>
2024-12-09 20:49:28 -0300

    fragment, div, array[], props

--- react_16/exercicios/src/index.js
+++ react_16/exercicios/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Primeiro from './componentes/Primeiro';
+//import Primeiro from './componentes/Primeiro';
+import BomDia from './componentes/BomDia';
 
 // renderizar primeiro elemento
-ReactDOM.render(<Primeiro />, document.getElementById("root"));
+//ReactDOM.render(<Primeiro />, document.getElementById("root"));
+
+ReactDOM.render(<BomDia nome="Camila" idade={10} />, document.getElementById("root"));
+
