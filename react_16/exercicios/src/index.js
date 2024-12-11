a93eaf1c630d462876f0e77c81a688ad87d99dac
Camila <alvescamila87@gmail.com>
2024-12-10 22:20:07 -0300

    export default

--- react_16/exercicios/src/index.js
+++ react_16/exercicios/src/index.js
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 //import Primeiro from './componentes/Primeiro';
 import BomDia from './componentes/BomDia';
+import { BoaTarde, BoaNoite } from './componentes/Multiplos'; 
 
 // renderizar primeiro elemento
 //ReactDOM.render(<Primeiro />, document.getElementById("root"));
 
-ReactDOM.render(<BomDia nome="Camila" idade={10} />, document.getElementById("root"));
+ReactDOM.render(
+    <div>
+        <BomDia nome="Camila" idade={10} />
+        <BoaTarde nome={"Zebedeu"} />
+        <BoaNoite nome={"Madalena"} />
+    </div>
+, document.getElementById("root"));
 
