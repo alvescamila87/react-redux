234eca2afd686a293ad0b430978246c33b827490
Camila <alvescamila87@gmail.com>
2024-12-15 21:43:05 -0300

    children throught props

--- react_16/exercicios/src/componentes/Pai.jsx
+++ react_16/exercicios/src/componentes/Pai.jsx
@@ -1,12 +1,22 @@
 import React from "react";
-import Filho from "./Filho";
+//import Filho from "./Filho";
+
+function childrenWithProps(props) {
+    return React.Children.map(props.children, child => {
+        return React.cloneElement(child, {
+            ...props, ...child.props
+        })
+    })
+}
 
 export default props => <div>
     <h1>{props.nome} {props.sobrenome}</h1>
     <h2>Filhos</h2>
     <ul>
-        <Filho nome="Zebedeu" sobrenome={props.sobrenome}/>
+        {/* <Filho nome="Zebedeu" sobrenome={props.sobrenome}/>
         <Filho {...props}/>
-        <Filho {...props}  nome="Madalena" />
+        <Filho {...props}  nome="Madalena" /> */}
+        {/* {props.children} */}
+        {childrenWithProps(props)}
     </ul>
 </div>
--- react_16/exercicios/src/index.js
+++ react_16/exercicios/src/index.js
@@ -5,6 +5,7 @@ import BomDia from './componentes/BomDia';
 import { BoaTarde, BoaNoite } from './componentes/Multiplos'; 
 import Saudacao from './componentes/Saudacao';
 import Pai from './componentes/Pai';
+import Filho from  './componentes/Filho';
 
 // renderizar primeiro elemento
 //ReactDOM.render(<Primeiro />, document.getElementById("root"));
@@ -23,7 +24,14 @@ ReactDOM.render(
     </div>
     <div>
         <h3>Aula Pai e Filho componentes</h3>
-        <Pai nome="Davi" sobrenome="Abraão"/>
+        <Pai nome="Davi" sobrenome="Abraão">
+            <Filho nome="Zebedeu" sobrenome="Silva"/>
+            {/* <Filho nome="Madalena" sobrenome="Silva"/>
+            <Filho nome="Lorena" sobrenome="Silva"/> */}
+            <Filho nome="Madalena"/>
+            <Filho nome="Lorena"/>
+
+        </Pai>
     </div>
     </React.Fragment>
 , document.getElementById("root"));
