f5df528e77f8a4854d1dcd03bdc67c0d8193b9c1
Camila Alves <alvescamila87@gmail.com>
2024-12-21 19:57:05 -0300

    header

--- react_16/crud/frontend/src/components/template/Logo.jsx
+++ react_16/crud/frontend/src/components/template/Logo.jsx
@@ -5,6 +5,6 @@ import logo from "../../assets/images/logo.jpg"
 export default props => 
     <aside className='logo'>
         <a href="/" className="logo">
-            <img src={logo} alt="logo" height={150}/>
+            <img src={logo} alt="logo" height={100}/>
         </a>
     </aside>
