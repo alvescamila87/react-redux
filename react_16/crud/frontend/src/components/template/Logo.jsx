f43fe1a65c2aab48ec410b2ab2346728eeaecafc
Camila Alves <alvescamila87@gmail.com>
2024-12-25 20:57:48 -0300

    user extends component

--- react_16/crud/frontend/src/components/template/Logo.jsx
+++ react_16/crud/frontend/src/components/template/Logo.jsx
@@ -1,10 +1,16 @@
 import './Logo.css'
 import React from 'react';
-import logo from "../../assets/images/logo.jpg"
+import logo from "../../assets/images/logo1.png"
+//import logo from "../../assets/images/logo2.png"
 
 export default props => 
     <aside className='logo'>
         <a href="/" className="logo">
-            <img src={logo} alt="logo" height={100}/>
+            <img 
+            src={logo} 
+            alt="logo" 
+            //height={100}
+
+            />
         </a>
     </aside>
