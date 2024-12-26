f39c1d15af11c75af033295d5577cde1a19cca87
Camila Alves <alvescamila87@gmail.com>
2024-12-26 20:06:47 -0300

    router, link, redirect, browser router

--- react_16/crud/frontend/src/components/template/Logo.jsx
+++ react_16/crud/frontend/src/components/template/Logo.jsx
@@ -2,15 +2,16 @@ import './Logo.css'
 import React from 'react';
 import logo from "../../assets/images/logo1.png"
 //import logo from "../../assets/images/logo2.png"
+import { Link } from 'react-router-dom'
 
 export default props => 
     <aside className='logo'>
-        <a href="/" className="logo">
+        <Link to="/" className="logo">
             <img 
             src={logo} 
             alt="logo" 
             //height={100}
 
             />
-        </a>
+        </Link>
     </aside>
--- react_16/crud/frontend/src/main/App.jsx
+++ react_16/crud/frontend/src/main/App.jsx
@@ -2,17 +2,19 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import "./App.css"
 import React from "react";
+import { BrowserRouter } from 'react-router-dom';
 
+import Routes from './Routes'
 import Footer from "../components/template/Footer";
 import Logo from "../components/template/Logo";
-//import Main from "../components/template/Main";
 import Nav from "../components/template/Nav";
-import Home from "../home/home";
 
 export default props =>
-    <div className="app">
-        <Logo />
-        <Nav />
-        <Home/>
-        <Footer />
-    </div>
+    <BrowserRouter>
+        <div className="app">
+            <Logo />
+            <Nav />
+            <Routes />
+            <Footer />
+        </div>
+    </BrowserRouter>
